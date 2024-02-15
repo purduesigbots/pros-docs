c74ac56da2fbe27280707cc9a1e11fc01155fc33
Andrew Lu <lu987@purdue.edu>
2024-02-15 15:32:45 -0500

    Hopefully Fix pros-4 location

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ echo Building project
 make clean all
 rm -r ./build/v5/pros-4
 mkdir ./build/v5/pros-4
-cp -r pros-doxygen-docs/ ./build/v5/pros-4
+cp -r pros-doxygen-docs/* ./build/v5/pros-4/
 
 mkdir artifacts
 pushd build
