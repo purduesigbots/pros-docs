79b51c84e60ecbf1db3e202be3bca77b2548a93e
12944qwerty <qwerty12944qwerty@gmail.com>
2024-05-24 03:50:13 -0500

    workflows

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,3 @@ make clean all
 rm -r ./build/v5/pros-4
 mkdir ./build/v5/pros-4
 cp -r pros-doxygen-docs/* ./build/v5/pros-4/
-
-mkdir artifacts
-pushd build
-tar -zcf ../artifacts/pros-docs.tar.gz *
-popd
