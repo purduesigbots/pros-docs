7fa3e38fe5dd6ff37e3c469606d82e38e9a8dfea
12944qwerty <qwerty12944qwerty@gmail.com>
2024-05-24 00:08:31 -0500

    maybe?

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-python=python
-echo Testing python executable version
-$python -c "import sys; exit(0 if sys.version_info > (3,6) else 1)"
-if [ $? -ne 0 ]
-then
-    python=python3.6
-fi
-
-echo Installing requirements.txt
-$python -m pip install -r requirements.txt
-
 echo Building project
 make clean all
 rm -r ./build/v5/pros-4
